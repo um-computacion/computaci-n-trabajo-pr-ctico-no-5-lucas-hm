97fa6fe4f64e2e732367db40ed533c16f5497f76
lucas-hm <lucashugomoran12@gmail.com>
2025-04-28 08:34:41 -0300

    etapa 2: comportamiento persona

--- src/persona.py
+++ src/persona.py
@@ -8,3 +8,11 @@ class Persona:
 
     def __repr__(self):
         return f"Persona: DNI: {self.dni} Nombre: {self.nombre} Apellido: {self.apellido} Ultima Idea: {self.ultima_idea}"
+    
+    def pensar(self, idea):
+        self.pensamientos += 1
+        self.ultima_idea = idea
+        
+    def pensar(self, idea):
+        self.pensamientos += 1
+        self.ultima_idea = idea
